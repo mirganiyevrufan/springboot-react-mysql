bfe4ffbeb066430a3e64fe6c113119bd28240d6d
Rufan Mirganiyev <109354732+rufanmirganiyev@users.noreply.github.com>
2023-07-08 17:26:39 -0400

    Delete fullstack-backend directory

--- fullstack-backend/src/main/java/com/rufan/fullstackbackend/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rufan.fullstackbackend.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-	public UserNotFoundException(Long id) {
-		super("Could not find the user with id " + id);
-	}
-}
